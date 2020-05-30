decae526d924199b179dacfb238b5906d74fc154
Klaus <38099918+zzzkeil@users.noreply.github.com>
2020-05-30 17:56:32 +0200

    Update generate_blocklist.sh

--- blocklist/generate_blocklist.sh
+++ blocklist/generate_blocklist.sh
@@ -10,11 +10,12 @@ wget -O  /root/adblocklists/anudeepNDads.txt https://raw.githubusercontent.com/a
 wget -O  /root/adblocklists/anudeepNDcoin.txt https://raw.githubusercontent.com/anudeepND/blacklist/master/CoinMiner.txt
 wget -O  /root/adblocklists/anudeepNDfacebook.txt https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt
 
-cat /root/adblocklists/StevenBlack.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock.list
-cat /root/adblocklists/bigdargon.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock.list
-cat /root/adblocklists/jdlingyu.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock.list
-cat /root/adblocklists/anudeepNDads.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock.list
-cat /root/adblocklists/anudeepNDcoin.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock.list
-cat /root/adblocklists/anudeepNDfacebook.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock.list
+cat /root/adblocklists/StevenBlack.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock_temp.list
+cat /root/adblocklists/bigdargon.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock_temp.list
+cat /root/adblocklists/jdlingyu.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock_temp.list
+cat /root/adblocklists/anudeepNDads.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock_temp.list
+cat /root/adblocklists/anudeepNDcoin.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock_temp.list
+cat /root/adblocklists/anudeepNDfacebook.txt | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' >> /root/adblocklists/unboundadblock_temp.list
 
-sort /root/adblocklists/unboundadblock.list | uniq -u
+awk '!visited[$0]++' /root/adblocklists/unboundadblock_temp.list > /root/adblocklists/unboundadblock.list
+rm /root/adblocklists/unboundadblock_temp.list
